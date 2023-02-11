0b78a5c7dc0f4951fec9aa0a83b14c0f988634df
paullaster <paullasterokoth98@gmail.com>
2023-02-12 00:11:49 +0300

    commit contact store files

---
+++ src/errorPae.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useRouteError } from 'react-router-dom'
+
+const errorPae = () => {
+  const error = userouteError();  
+  return (
+    <div>errorPae</div>
+  )
+}
+
+export default errorPae
