20a9c5dad1f5a4df817c0707ce2d007085e3a90c
paullaster <paullasterokoth98@gmail.com>
2023-02-12 09:41:56 +0300

    adding contact routes files

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />
   },
   {
-    path: '/contact/:contactId',
+    path: 'contact/:contactId',
     element: <contact />,
   }
 ])
