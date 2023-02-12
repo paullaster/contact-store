f7bfbb916b60fd3364d337beffab198411d4900c
paullaster <paullasterokoth98@gmail.com>
2023-02-12 23:30:58 +0300

    commit files

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -27,7 +27,7 @@ const root = () => {
             <div id="sidebar">
               <h1>React Router Contacts</h1>
               <div>
-                <form id="search-form" role="search">
+                <Form id="search-form" role="search">
                   <input
                     id="q"
                     aria-label="Search contacts"
@@ -44,7 +44,7 @@ const root = () => {
                     className="sr-only"
                     aria-live="polite"
                   ></div>
-                </form>
+                </Form>
                 <Form method="post">
                   <button type="submit">New</button>
                 </Form>
