ab9b73ae90fd952e0f94fa5b4dec3b5ec9f0c3ec
paullaster <paullasterokoth98@gmail.com>
2023-02-12 09:37:56 +0300

    adding contact routes files

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -69,4 +69,25 @@ const contact = () => {
       );
 }
 
+function Favorite({ contact }) {
+    // yes, this is a `let` for later
+    let favorite = contact.favorite;
+    return (
+      <Form method="post">
+        <button
+          name="favorite"
+          value={favorite ? "false" : "true"}
+          aria-label={
+            favorite
+              ? "Remove from favorites"
+              : "Add to favorites"
+          }
+        >
+          {favorite ? "★" : "☆"}
+        </button>
+      </Form>
+    );
+  }
+
+
 export default contact
