a413528e1192dcc7cb547fb9f38b95b605a85bc4
paullaster <paullasterokoth98@gmail.com>
2023-02-12 11:16:40 +0300

    adding contact routes files

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -37,14 +37,7 @@ const root = () => {
                 </form>
               </div>
               <nav>
-                <ul>
-                  <li>
-                    <Link to={`/contacts/1`}>Your Name</Link>
-                  </li>
-                  <li>
-                    <Link to={`/contacts/2`}>Your Friend</Link>
-                  </li>
-                </ul>
+                
               </nav>
             </div>
             <div id="detail">
