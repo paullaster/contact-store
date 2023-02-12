392fa4b8d722991b2ff326145b1f7ebf7ca3f731
paullaster <paullasterokoth98@gmail.com>
2023-02-12 13:08:24 +0300

    adding contact routes files

--- src/main.jsx
+++ src/main.jsx
@@ -34,6 +34,7 @@ const router = createBrowserRouter([
       {
         path: 'contacts/:contactId/edit',
         element: <EditContact />,
+        action: editContactAction,
         loader: editContactLoader,
       }
     ]
