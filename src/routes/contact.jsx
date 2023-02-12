7b5c300d5f7c63acfac2ef87de5a0b281a4fd2fc
paullaster <paullasterokoth98@gmail.com>
2023-02-12 12:16:33 +0300

    adding contact routes files

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -7,14 +7,14 @@ export const loader = async ({params}) => {
 
 export default function Contact() {
   const contact = useLoaderData ();
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placekitten.com/g/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+//   const contact = {
+//     first: "Your",
+//     last: "Name",
+//     avatar: "https://placekitten.com/g/200/200",
+//     twitter: "your_handle",
+//     notes: "Some notes",
+//     favorite: true,
+//   };
 
   return (
     <div id="contact">
