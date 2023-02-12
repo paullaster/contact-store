e031172ac111ce3d1811be4b4fbc81affe7f0eeb
paullaster <paullasterokoth98@gmail.com>
2023-02-12 21:57:25 +0300

    commit files

--- src/routes/deleteContact.jsx
+++ src/routes/deleteContact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { deleteContact } from '../Contact'
+import { redirect } from 'react-router-dom'
 
-export const action = async () => {
-    
+export const action = async ({params}) => {
+    await deleteContact(params.contactId)
+    return redirect ("/");
 }
