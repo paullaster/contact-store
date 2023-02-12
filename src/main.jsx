13f01dcb01b36356044a04195d7a71a454ddbb70
paullaster <paullasterokoth98@gmail.com>
2023-02-12 13:08:07 +0300

    adding contact routes files

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import Contact, {
   loader as contactLoader,
 } from './routes/contact'
 import EditContact, {
+  action as editContactAction,
   loader as editContactLoader,
 } from './routes/editContact'
 
