2ead4629df2530dbd25b134e2c8132997ef796b8
paullaster <paullasterokoth98@gmail.com>
2023-02-12 21:55:04 +0300

    commit files

--- src/routes/deleteContact.jsx
+++ src/routes/deleteContact.jsx
@@ -1,10 +1,3 @@
 import React from 'react'
 import { deleteContact } from '../Contact'
 
-const deleteContact = () => {
-  return (
-    <div>deleteContact</div>
-  )
-}
-
-export default deleteContact
